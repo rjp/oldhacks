c76da212f8915a4f6a18049f127a6d239ff5ee7b
rjp@papaloctopus.lan <>
2008-01-11 01:11:41 +0000

    - almost total rewrite to do things in a correct Shoes-way - draw the board using stacks, flows, and images - implement the XML fetching for drawing a game - we have to reset the board between each display - TODO: slow!

--- scrabble.rb
+++ scrabble.rb
@@ -2,48 +2,22 @@ require 'rubygems'
 require 'hpricot'
 require 'open-uri'
 
-game_id = 13386865
+$server = '74.54.87.124'
+$squares = Array.new(15) {Array.new(15)}
 
-server = '74.54.87.124'
-rand_val = rand()
-game_url = %Q[http://#{server}/email_scrabble/xmlv3.php?showGameOver=&gid=#{game_id}&action=gameinfo&rnd=#{rand_val}]
-
-def draw_board 
-    0.upto(14) do |x|
-        0.upto(14) do |y|
-            fill rgb(1.0,1.0,1.0)
-            stroke rgb(0.0,0.0,0.0)
-            rect 34*x+17, 34*y+17, 34, 34
-        end
-    end
-    { 
-        [1,0,0] => [[0,0],[0,7],[0,14],[7,0]],
-        [0.8,0.8,1] => [[0,3],[3,0],[2,6],[6,2],[7,3],[6,6]],
-        [0.4,0.4,1] => [[1,5],[5,1],[5,5]],
-        [1,0.8,0.8] => [[1,1],[2,2],[3,3],[4,4],[7,7]]
-    }.each { |k,v|
-        stroke rgb(0.0,0.0,0.0)
-        f = k.map{|p|p.to_f}
-        fill rgb(*f)
-        v.each { |x,y|
-            rect 34*x+17, 34*y+17, 34, 34
-            rect 34*y+17, 34*x+17, 34, 34
-            rx,ry = 14-x,14-y
-            rect 34*rx+17, 34*ry+17, 34, 34
-            rect 34*ry+17, 34*rx+17, 34, 34
-            rect 34*rx+17, 34*y+17, 34, 34
-            rect 34*x+17, 34*ry+17, 34, 34
+def reset_board(tiles, squares)
+    tiles.each_with_index { |i,x|
+        i.each_with_index { |p,y|
+            squares[x.to_i][y.to_i].path = "#{p}.png"
+            squares[x.to_i][y.to_i].show
         }
     }
 end
 
-# doc = Hpricot.XML(open(game_url))
-Shoes.app :height => 34*16, :width => 34*16 do
-
-strokewidth(2)
-draw_board
-
-doc = Hpricot.XML(open('endgame.xml'))
+def update_board(game_id, squares)
+rand_val = rand()
+game_url = %Q[http://#{$server}/email_scrabble/xmlv3.php?showGameOver=&gid=#{game_id}&action=gameinfo&rnd=#{rand_val}]
+doc = Hpricot.XML(open(game_url)) # 'endgame.xml'))
 gi = {}
 doc.search("/xml/gameinfo/*").each { |i|
     gi[i.name] = i.inner_text
@@ -70,19 +44,55 @@ moves[1..-1].each_with_index { |j, move|
         letter,x,y,f = nodes[node]
         if letter == ch then
             node = node + 1
-            xc = 34*y.to_i + 18
-            yc = 34*x.to_i + 18
-# puts "#{player} placed #{letter} at #{x},#{y}"
-            if letter.upcase == letter then
-                o = image "#{letter}.png", :left => xc, :top => yc
-            else
-                o = image "#{letter.upcase}.png", :left => xc, :top => yc
-                nofill
-                strokewidth(3)
-                stroke rgb(1.0,0.0,0.0) 
-                rect xc-2, yc-2, 34, 34
-            end
+#            if letter.upcase == letter then
+            squares[x.to_i][y.to_i].path = "#{letter}.png"
+            squares[x.to_i][y.to_i].show
+#            else
+#            end
         end
     }
+}
+end
+
+# doc = Hpricot.XML(open(game_url))
+Shoes.app :height => 34*17, :width => 34*15 do
+    tiles = Array.new(15) {Array.new(15,'white')}
+    { 
+        'red' => [[0,0],[0,7],[0,14],[7,0]],
+        'aquamarine' => [[0,3],[3,0],[2,6],[6,2],[7,3],[6,6]],
+        'blue' => [[1,5],[5,1],[5,5]],
+        'pink' => [[1,1],[2,2],[3,3],[4,4],[7,7]]
+    }.each { |k,v|
+        v.each { |x,y|
+            rx,ry = 14-x,14-y
+            tiles[x][y] = k
+            tiles[y][x] = k
+            tiles[rx][ry] = k
+            tiles[ry][rx] = k
+            tiles[rx][y] = k
+            tiles[x][ry] = k
+        }
+    }
+    
+stack :margin => 10 do
+    flow do
+        game_id = edit_line :text => 'Game ID'
+        button "Display" do
+            reset_board tiles, $squares
+            update_board game_id.text, $squares
+            nofill
+            fill rgb(1.0,1.0,1.0)
+        end
+    end
+end
+
+tiles.each_with_index { |i,x|
+    stack :width => 15*32 do
+        flow do
+	        i.each_with_index { |p,y|
+	            $squares[x.to_i][y.to_i] = image "#{p}.png"
+	        }
+        end
+    end
 }
 end
