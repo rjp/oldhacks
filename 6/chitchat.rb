b5119f29599121c6c60fa88090c1df6990338225
rjp@papaloctopus.lan <>
2008-01-15 23:29:18 +0000

    - move file parsing into Shoes.app so we can use a file dialog later - add some more colours to fit #moo in nicely - make lots of globals until I figure out how Shoes scoping works - use a fixed height window since we can't size it based on the file - move away from clear() each time in favour of replace() and append() - display usernames in their allocated colours - stop animating at 23:59, not 24:00 (which looks stupid)

--- 6/chitchat.rb
+++ 6/chitchat.rb
@@ -1,28 +1,30 @@
 require 'time'
 
-day_start = Time.parse(ARGV[1]).to_i
-day_end = day_start + 86400
+$day_start = Time.parse(ARGV[1]).to_i
+$day_end = $day_start + 86400
 
-slots = Array.new(288)
-blocks = Array.new(1440)
-colourmap = Hash.new()
+$slots = Array.new(288)
+$blocks = Array.new(1440)
+$colourmap = Hash.new()
 
-colours = [ 
+$colours = [ 
     [0.0,0.0,1.0], [1.0,0.0,0.0], [1.0,1.0,0.0],
-    [1.0,0.0,1.0], [0.0,1.0,1.0], [0.0,1.0,0.0]
+    [1.0,0.0,1.0], [0.0,1.0,1.0], [0.0,1.0,0.0],
+    [0.5,0.5,0.5], [0.5,1.0,0.5], [1.0,0.5,0.5]
 ]
 
-
 # colours = ['red', 'blue', 'green', 'purple', 'yellow', 'orange']
-cindex = 0
+$cindex = 0
 
-File.open(ARGV[2]).readlines.each { |i| 
+def parse_file(filename)
+
+File.open(filename).readlines.each { |i| 
 	next unless i =~ /\d+ ..:.. (<|\*)/
 
 	ts, hm, who, who2, junk = i.gsub(/[<>]/,'').split(' ', 5)
 
-	break if ts.to_i >= day_end
-	next if ts.to_i < day_start
+	break if ts.to_i >= $day_end
+	next if ts.to_i < $day_start
 
 	if (who == '*') then who = "<#{who2}>"; end
     who.gsub!(/[<>@ ]/, '')
@@ -32,57 +34,55 @@ File.open(ARGV[2]).readlines.each { |i|
 	offset = 12*hours.to_i + (minutes.to_i/5)
 	daymin = 60*hours.to_i + minutes.to_i
 
-    if colourmap[who].nil? then
-        colourmap[who] = colours[cindex]
-        cindex = (cindex + 1) % colours.size
+    if $colourmap[who].nil? then
+        $colourmap[who] = [$cindex, $colours[$cindex]]
+        $cindex = ($cindex + 1) % $colours.size
     end
 
-	if (slots[offset].nil?) then slots[offset] = []; end
-	if (blocks[daymin].nil?) then blocks[daymin] = []; end
-	blocks[daymin].push [60*hours.to_i+minutes.to_i, colourmap[who], 2*offset, 2*slots[offset].size]
-	slots[offset].push [hm, colourmap[who]]
+	if ($slots[offset].nil?) then $slots[offset] = []; end
+	if ($blocks[daymin].nil?) then $blocks[daymin] = []; end
+	$blocks[daymin].push [60*hours.to_i+minutes.to_i, $colourmap[who][1], 2*offset, 2*$slots[offset].size]
+	$slots[offset].push [hm, $colourmap[who][1]]
 }
-max = -1
-slots.each { |i| unless (i.nil?) then if (i.size > max) then max = i.size; end; end }
 
-height = 20+max*2
+    return $blocks, $slots, $colourmap
+end
 
-$todraw = []
-
-Shoes.app :width => 576, :height => height do
+Shoes.app :width => 576, :height => 150 do
+    height = 150
 	minutes = 360 
 	index = 0
 
+    blocks, slots, users = parse_file(ARGV[2])
+    
+    timecount = nil
+    stack do
+        flow do 
+            timecount = para '00:00'
+            users.sort_by{|v| v[1][0]}.each { |i|
+                para i[0], :stroke => rgb(*(i[1][1]))
+            }
+        end
+    end
+
 	animate(15) do
-		if minutes < 1441 then
-		clear do
-				h = minutes / 60
-				m = minutes % 60
+		if minutes < 1440 then
+			h = minutes / 60
+			m = minutes % 60
 
-				stack do
-					para sprintf '%02d:%02d', h, m
-				end
+			timecount.replace (sprintf '%02d:%02d', h, m)
 	
-				unless blocks[minutes].nil? then
-					blocks[minutes].each {|i|
-						m, c, x, y = *i
-						$todraw.push [x, height-y-8, c]
-					}
-				end
-
-				$todraw.each { |i|
-					x, y, c = *i
-					nostroke
-                    if c.is_a?(Array) then
-    					fill rgb(*c)
-                    else
-                        fill c
+			unless blocks[minutes].nil? then
+				blocks[minutes].each {|i|
+					m, c, x, y = *i
+                    append do
+	                    nostroke
+	                    fill rgb(*c)
+	                    rect x, height-y-8, 2, 2
                     end
-					rect x, y, 2, 2
-				}
-					
+        		}
+	    	end
 			minutes = minutes + 1
-		end
 		end
 	end
 end
