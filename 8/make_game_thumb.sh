3ccbcdca13eada6a24e382657b8f50cb98ef863d
rjp@PAPALOCTOPUS <>
2008-01-23 00:10:15 +0000

    - working update - proper blankboard, aquamarine replaced with skyblue - FIX: letters were rotated 90 degrees - silence ed's STDERR bleating - 128x128 is much more legible - add a frame to the tiles for nice visual separation

--- 8/make_game_thumb.sh
+++ 8/make_game_thumb.sh
@@ -1,22 +1,24 @@
 across=15
 down=15
 gameid=$1
-letters=../letters/
+letters=letters/
 
-cp blankboard newboard
+# yes letters/grey90.png | head -225 > newboard
+sed -e "s#^#$letters#" < blankboard > newboard
 
-nodes='Z,1,2,3|Q,1,3,4|r,0,0,5'
-# nodes=$(echo "select nodes from game where gameid=$gameid" | sqlite3 /tmp/scrabgames.db)
+# nodes='Z,1,2,3|Q,1,3,4|r,0,0,5'
+echo "select nodes from game where gameid=$gameid;"
+nodes=$(echo "select nodes from game where gameid=$gameid;" | sqlite3 /tmp/scrabgames.db)
 
 for tile in $(echo $nodes | tr '|' ' '); do
 	set -- $(echo $tile | tr ',' ' ')
 	letter=$1
 	x=$2
 	y=$3
-	line=$((15*y+x))
+	line=$((15*x+y))
 	color=white
 	uc=$(echo $letter | tr a-z A-Z)
-	if [ $uc != $letter ]; then color=blue; fi
+	if [ $uc != $letter ]; then color=red; fi
 	echo "placing letter $letter at $x,$y (line $line)"
 	(
 		echo $((line+1))d
@@ -25,8 +27,8 @@ for tile in $(echo $nodes | tr '|' ' '); do
 		echo .
 		echo w
 		echo q
-	) | ed newboard
+	) | ed newboard 2>/dev/null
 done
 
 montage +frame +shadow +label -tile 15x15 -geometry 32x32 @newboard board.png
-convert -resize 96x96 board.png $gameid.png
+convert -resize 128x128 board.png $gameid.png
