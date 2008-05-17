1132dbc8c844318630e6faa152193358246f4b04
rjp@papaloctopus.lan <>
2008-05-17 08:14:26 +0100

    - preparse the results into per-day yaml

--- 11/parseprem.rb
+++ 11/parseprem.rb
@@ -3,16 +3,24 @@ require 'hpricot'
 
 # takes a Hpricot doc
 def parse_all(doc)
-	matches = []
+	matches = Hash.new {|h,k| h[k] = []}
+    match_date = nil
     doc.search('//table[@class="competitionResults"]/tr/comment()/..').each { |match| 
         title = match.inner_text.strip.gsub(%r{[\n\t\r ]+}, ' ')
-		matches.push title
+        d = match.parent.previous_sibling.previous_sibling
+        if d.nil? then 
+            puts "no previous sibling sibling for #{title}"
+            exit
+        elsif d.name == 'b' then
+            match_date = d.inner_text
+        end
+        matches[match_date].push title
 	}
     return matches
 end
 
 def parse_file(filename)
-	doc = Hpricot(open(filename))
+    doc = open(filename) { |f| Hpricot f, :fixup_tags => true }
 	return parse_all(doc)
 end
 
