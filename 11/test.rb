8e88a2c1c8545dcf739b2ca87af78c6c13f7b445
rjp@papaloctopus.lan <>
2008-05-17 08:39:42 +0100

    - use results.yaml, update table after every set of games

--- 11/test.rb
+++ 11/test.rb
@@ -1,12 +1,14 @@
-require 'parseprem'
+require 'yaml'
 require ARGV[0]
 
-table = Hash.new { |h,k| h[k] = {:win=>0, :draw=>0, :lose=>0, :played=>0, :points=>0, :bonus=>0} }
+table = Hash.new { |h,k| h[k] = {:win=>0, :draw=>0, :lose=>0, :played=>0, :points=>0, :bonus=>0, :pos=>0} }
 
-games = parse_file('results.html')
-games.reverse.each { |game|
+by_date = YAML.load_file('results.yaml')
+by_date.each { |date, games|
+games.each { |game|
 # Birmingham 4-1 (HT 1-0) Blackburn
     all, home, hs, as, away = game.match(%r{(.*?) (\d+)-(\d+) \(.*?\) (.+)}).to_a
+    puts "h=#{home} a=#{away} s=#{hs}-#{as}"
     if hs > as then
         hp, hb, ap, ab = points_for_home_win(hs, as, 0, 0)
         table[home][:points] = table[home][:points] + hp
@@ -31,7 +33,11 @@ games.reverse.each { |game|
     table[home][:bonus] = table[home][:bonus] + hb
     table[away][:bonus] = table[away][:bonus] + ab
 }
-printf "%2s %-20s %3s %4s %3s\n", '#', 'Team', 'Pts', 'Bns', 'Tot'
-table.keys.sort_by {|h| table[h][:points]+table[h][:bonus] }.reverse.each_with_index {|t,i|
-    printf "%2d %-20s %3d +%3d %3d\n", i+1, t, table[t][:points], table[t][:bonus], table[t][:points]+table[t][:bonus]
+    simple = []
+    table.keys.sort_by {|h| table[h][:points]+table[h][:bonus] }.reverse.each_with_index {|t,i| table[t][:pos] = i+1; simple.push "#{t} (#{table[t][:points]+table[t][:bonus]})" }
+    puts "#{date}: " << simple[0..3].join(',')
+}
+printf "%2s %-20s %3s %4s %3s %s\n", '#', 'Team', 'Pts', 'Bns', 'Tot', 'pos'
+table.keys.sort_by{|k|table[k][:pos]}.each_with_index {|t,i|
+    printf "%2d %-20s %3d +%3d %3d %3d\n", i+1, t, table[t][:points], table[t][:bonus], table[t][:points]+table[t][:bonus], table[t][:pos]
 }
