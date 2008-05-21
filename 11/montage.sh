3576c0d5d07dad4bb8b6340e37c4aa423b76467a
rjp@backup.frottage.org.frottage.org <>
2008-05-21 15:51:38 +0100

    - spread_zero is a nicer graphic

--- 11/montage.sh
+++ 11/montage.sh
@@ -1,2 +1,3 @@
-for i in svg/*; do convert $i ppm:- | pnmcrop | pnmpad -l 2 -r 2 -white | pnmtopng > png/$(basename $i).png; done
+for i in svg/*; do convert $i ppm:- | pnmcrop | pnmpad -t 2 -b 2 -l 2 -r 2 -white | pnmtopng > png/$(basename $i).png; done
 montage -tile x1 -geometry x201 png/*spread.svg.png test.png
+montage -tile x1 -geometry x201 png/*spread_zero.svg.png test2.png
