8185c438d2ee55c11162801d186cd9e02b1aad77
user@asus-1552752036 <>
2008-05-27 08:47:36 +0100

    - store history and graphs in subdirectories

--- 11/driver.sh
+++ 11/driver.sh
@@ -1,12 +1,16 @@
+prefix=${PREFIX:-games}
+
+mkdir -p history/$prefix svg/$prefix png/$prefix
+
 for i in rules/*.rb; do
 	j=${i%.*}
 	j=$(basename $j)
-	echo XXX ruby test.rb $i history/$j
-	ruby test.rb $i history/$j
+	echo XXX ruby test.rb $i history/$prefix/$j
+	ruby test.rb $i history/$prefix/$j
 	for k in graphs/*.rb; do
 		l=${k%.*}
 		l=$(basename $l)
-		ruby $k history/$j > svg/${j}_$l.svg
+		ruby $k history/$prefix/$j > svg/$prefix/${j}_$l.svg
 	done
 done
 
