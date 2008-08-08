51ec522a6687b0cdbf45a934464ac48d89875fea
user@asus-1552752036 <>
2008-08-08 17:29:33 +0100

    - count and display win/lose/draw numbers

--- 11/test.rb
+++ 11/test.rb
@@ -13,6 +13,8 @@ def home_win(table, home, away, hs, as)
         table[home][:bonus] = table[home][:bonus] + hb
         table[away][:points] = table[away][:points] + ap
         table[away][:bonus] = table[away][:bonus] + ab
+	table[home][:win] = table[home][:win] + 1
+	table[away][:lose] = table[away][:lose] + 1
 end
 
 def away_win(table, home, away, hs, as)
@@ -21,6 +23,8 @@ def away_win(table, home, away, hs, as)
         table[home][:bonus] = table[home][:bonus] + hb
         table[away][:points] = table[away][:points] + ap
         table[away][:bonus] = table[away][:bonus] + ab
+	table[away][:win] = table[away][:win] + 1
+	table[home][:lose] = table[home][:lose] + 1
 end
 
 def draw(table, home, away, hs, as)
@@ -29,6 +33,8 @@ def draw(table, home, away, hs, as)
         table[home][:bonus] = table[home][:bonus] + hb
         table[away][:points] = table[away][:points] + ap
         table[away][:bonus] = table[away][:bonus] + ab
+	table[away][:draw] = table[away][:draw] + 1
+	table[home][:draw] = table[home][:draw] + 1
 end
 
 by_date = YAML.load_file('results.yaml')
@@ -38,6 +44,8 @@ games.each { |game|
     all, home, hs, as, away = game.match(%r{(.*?) (\d+)-(\d+) \(.*?\) (.+)}).to_a
     hs = hs.to_i
     as = as.to_i
+    table[home][:played] = table[home][:played]+1
+    table[away][:played] = table[away][:played]+1
     puts "h=#{home} a=#{away} s=#{hs}-#{as}"
     if hs > as then
     	home_win(table, home, away, hs, as)
@@ -61,7 +69,7 @@ games.each { |game|
     table.keys.sort_by {|x| sort_routine.call(x)}.reverse.each_with_index {|t,i| table[t][:pos] = i+1; simple.push "#{t} (#{table[t][:points]+table[t][:bonus]})" }
     puts "#{date}: " << simple[0..3].join(',')
 }
-printf "%2s %-20s %3s %4s %3s %s %3s %3s %4s\n", '#', 'Team', 'Pts', 'Bns', 'Tot', 'pos', 'gf', 'ga', 'gd'
+printf "%2s %-16s %3s %4s %3s %s %3s %3s %4s %2s %2s %2s %2s\n", '#', 'Team', 'Pts', 'Bns', 'Tot', 'pos', 'gf', 'ga', 'gd', 'P', 'W', 'D', 'L'
 table.keys.sort_by{|k|table[k][:pos]}.each_with_index {|t,i|
-    printf "%2d %-20s %3d +%3d %3d %3d %3d %3d %4d\n", i+1, t, table[t][:points], table[t][:bonus], table[t][:points]+table[t][:bonus], table[t][:pos], table[t][:for], table[t][:against], table[t][:for]-table[t][:against]
+    printf "%2d %-16s %3d +%3d %3d %3d %3d %3d %4d %2d %2d %2d %2d\n", i+1, t, table[t][:points], table[t][:bonus], table[t][:points]+table[t][:bonus], table[t][:pos], table[t][:for], table[t][:against], table[t][:for]-table[t][:against], table[t][:played], table[t][:win], table[t][:draw], table[t][:lose]
 }
