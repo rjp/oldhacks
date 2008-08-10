7451bfec9b29e9954dee15afbdb89c8751756191
user@asus-1552752036 <>
2008-08-10 07:48:52 +0100

    - scale was inverted - forgot about no auto type coercion (again)

--- 12/ua_graph.rb
+++ 12/ua_graph.rb
@@ -1,6 +1,5 @@
 require 'svg/svg'
 
-
 spans = Hash.new {|h,k| h[k] = []}
 timeon = Hash.new(0)
 
@@ -8,17 +7,19 @@ min, max = 1000000000,-1
 
 $stdin.readlines.each { |line|
 	userid, on, off = line.split(' ')
+	on = on.to_i; off = off.to_i
 	spans[userid].push([on, off])
 	timeon[userid] = timeon[userid] + off - on
-	if min < on then min = on; end
+	if on < min then min = on; end
 	if off > max then max = off; end
 }
 
 puts "range=#{min}-#{max}"
-scale = (max-min)/800.0
+scale = 800.0/(max-min)
 
-spans.keys.sort_by {|k| timeon[k]}.each_with_index { |user, i|
+spans.keys.sort_by {|k| timeon[k]}.reverse.each_with_index { |user, i|
 	y = 4 + 2*i
+	puts "plotting #{user} at y=#{y}, s=#{scale}"
 	spans[user].each { |s,e|
 		xs = 10 + (s-min)*scale
 		xe = 10 + (e-min)*scale
