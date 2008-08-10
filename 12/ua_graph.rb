928323ca5a620a5553e24b156d384f0ee71f7426
user@asus-1552752036 <>
2008-08-10 07:52:18 +0100

    - add svg plotting

--- 12/ua_graph.rb
+++ 12/ua_graph.rb
@@ -17,12 +17,15 @@ $stdin.readlines.each { |line|
 puts "range=#{min}-#{max}"
 scale = 800.0/(max-min)
 
+svg = SVG.new('8in', '4in')
 spans.keys.sort_by {|k| timeon[k]}.reverse.each_with_index { |user, i|
-	y = 4 + 2*i
+	y = 4 + 3*i
 	puts "plotting #{user} at y=#{y}, s=#{scale}"
 	spans[user].each { |s,e|
 		xs = 10 + (s-min)*scale
 		xe = 10 + (e-min)*scale
 		puts "(#{xs},#{y})-(#{xe},#{y})"
+		svg << SVG::Line.new(xs, y, xe, y) { self.style = SVG::Style.new(:stroke_width => '2', :fill => 'none', :stroke => 'black') }
 	}
 }	
+$stderr.puts svg.to_s
