5ab38094bccb9a09731ef8546515effee17521a0
rjp@backup.frottage.org.frottage.org <>
2008-10-20 19:38:30 +0100

    - plot circles of dopplr-style colour for now

--- x9/plotpos.rb
+++ x9/plotpos.rb
@@ -1,7 +1,8 @@
 require 'yaml'
+require 'digest/md5'
 
 infohash = YAML.load(File.new(ARGV[0]))
-title = infohash['title']
+title = "League 2008/9: canspurswin"
 
 puts <<HEADER
 <?xml version="1.0" standalone="no"?>
@@ -13,3 +14,17 @@ puts <<HEADER
 <desc>League Positions vs Games Played</desc>
 <rect x="1" y="1" width="398" height="398" fill="none" stroke="blue" />
 HEADER
+
+start=7
+start.upto(37) { |i|
+    infohash.keys.each { |team|
+        colour = Digest::MD5.hexdigest(team)[0..5]
+        y = infohash[team][i] * 10
+        x = 10 + i * 10
+        puts %Q{<circle cx="#{x}" cy="#{y}" r="5" fill="##{colour}" stroke="black"/><!-- #{team}[#{i}]=#{infohash[team][i]} -->}
+    }
+}
+
+puts <<FOOTER
+</svg>
+FOOTER
