521134a78c04a24138f73153a5d14eaae3f088f3
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-02-26 09:05:26 +0000

    - having a teams.yaml is vital so try and generate one if it's missing - abort if we find fewer than 20 teams (mostly safe for English leagues) - TODO get this from a fixture list instead

--- 14/prem.sh
+++ 14/prem.sh
@@ -7,6 +7,17 @@ OPFX=data/${LGE}/${YEAR}
 mkdir -p data/${LGE} -p history/${LGE}/${YEAR} $OHTML
 
 # wget -O $OPFX.html http://news.bbc.co.uk/sport1/hi/football/eng_prem/results/default.stm
+
+if [ ! -e ${OPFX}_teams.yaml ]; then
+    echo "You have no teams file, trying to generate one from the results"
+    lynx -dump $OPFX.html | grep '(HT' | cut -d']' -f2 | sort -u | sed -e 's/ /_/g' -e 's/^/- /' > ${OPFX}_teams.yaml
+    teams=$(wc -l < ${OPFX}_teams.yaml)
+    if [ $teams < 20 ]; then
+        echo "Only found $teams teams in results, please fix up ${OPFX}_teams.yaml by hand"
+        exit
+    fi
+fi
+
 ruby preparse.rb $OPFX.html $OPFX.yaml
 LEAGUE=$OPFX.yaml PREFIX=$PPFX ./driver.sh
 HTML=1 ruby showfinal.rb history/$PPFX/* > $OHTML/test.xhtml
