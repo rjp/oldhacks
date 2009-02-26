59d1af330f6c4b171907e967833a1913b40330ce
rjp <zimpenfish@gmail.com>
2009-02-26 08:48:26 +0000

    - forgot an important file

---
+++ 14/preparse.rb
@@ -0,0 +1,27 @@
+require 'yaml'
+require 'parseprem'
+
+=begin rdoc
+Convert a BBC-style results list into a YAML file suitable for loading
+=end
+
+input = ARGV[0]
+output = ARGV[1] || 'results.yaml'
+
+table = Hash.new { 
+    |h,k| h[k] = {
+        :win=>0, :draw=>0, :lose=>0, :played=>0, :points=>0, :bonus=>0, :final=>0
+    } 
+}
+
+games, bookings = parse_file(input)
+dates = games.keys.sort_by{|h| Time.parse(h).to_i}
+by_date = []
+dates.each { |d| by_date.push [d, games[d]] }
+open(output, 'w') {|f|
+    YAML.dump(by_date, f)
+}
+bkfile = output.gsub(/.yaml$/, '_bk.yaml')
+open(bkfile, 'w') { |f|
+    bookings.each { |z| f.puts z }
+}
