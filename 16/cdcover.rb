880d3cd0d5e81ca65827d0fea63b04745faffbae
rjp <zimpenfish@gmail.com>
2009-03-16 10:14:36 +0000

    - checkpoint

--- 16/cdcover.rb
+++ 16/cdcover.rb
@@ -1,5 +1,4 @@
-require 'rubygems'
-require 'rmagick'
+require 'erb'
 
 # graph parameters
 size = 300
@@ -9,9 +8,6 @@ width = 0.9*size
 height = 0.4*size
 scale = 65536 / height
 midpoint = size - 1.1*height
-
-canvas = Magick::Image.new(size, size) { self.background_color = 'white' }
-gc = Magick::Draw.new
 
 max_vol = 65535
 
@@ -51,11 +47,14 @@ end
 
 buckets = Array.new(300) { Bucket.new }
 
-# read a 16 bit linear raw PCM file
+# sox -v 0.9 /path/to/test.mp3 -b 16 -s -c 2 -t au -
+
 puts "reading file ", Time.now
 x = IO.read(file)
 puts "unpacking file ", Time.now
-bytes = x.unpack("i*")
+type, dataloc, data = x.unpack("NNa*")
+puts "type=#{type} dloc=#{dataloc}"
+bytes = x[dataloc..-1].unpack('i*')
 puts "bucketing samples, ", Time.now
 bucket_size = bytes.size / width
 p bytes.size
@@ -70,23 +69,11 @@ bytes.each_with_index { |i,j|
     buckets[bucket].add(left)
 }
 puts "plotting graph ", Time.now
-buckets.each_with_index { |b, i|
-    gc.stroke('#000000')
-    if b.count > 0 then
-        avg_p, avg_n = b.avg
-        # 5 -6.80763244628906 8.06854248046875 -0.825119018554687 0.821914672851562 / -4249 -515 513 5036 70724 -88632
-#        puts "#{i} #{b.min/scale} #{b.max/scale} #{avg_n/scale} #{avg_p/scale} / #{b.min} #{avg_n} #{avg_p} #{b.max} #{b.count} #{b.total}"
-        low = (avg_n + b.min) / 2
-        high = (avg_p + b.max) / 2
-        gc.line(i+offset, midpoint+low/scale, i+offset, midpoint+high/scale)
-    end
-}
-# now plot some suitably sized text
-gc.pointsize = 14
-gc.stroke('#888888')
-gc.fill('#888888')
-gc.text(0.1*size, midpoint + 0.6*height, tracknum)
-gc.text(0.1*size, midpoint + 0.7*height, trackname)
-gc.draw(canvas)
-canvas.write("png/test.png")
+t_x = 0.1*size
+t_y1 = midpoint + 0.6*height
+t_y2 = midpoint + 0.7*height
+title = "Sonogram of MONKEY BADGER"
+template = File.read("output.erb")
+tt = ERB.new(template, nil, '%')
+$stderr.puts tt.result
 puts Time.now
