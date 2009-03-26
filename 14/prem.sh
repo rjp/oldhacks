54e96a552e8c5971aa699c7233671aa888b3ea68
rjp <zimpenfish@gmail.com>
2009-03-26 13:00:35 +0000

    - 14, double tr fix with extra log output

--- 14/prem.sh
+++ 14/prem.sh
@@ -6,7 +6,9 @@ OPFX=data/${LGE}/${YEAR}
 
 mkdir -p data/${LGE} -p history/${LGE}/${YEAR} $OHTML
 
-wget -O $OPFX.html http://news.bbc.co.uk/sport1/hi/football/eng_prem/results/default.stm
+#wget -O $OPFX.html http://news.bbc.co.uk/sport1/hi/football/eng_prem/results/default.stm
+#wget -O tmp.html http://news.bbc.co.uk/sport1/hi/football/eng_prem/results/default.stm
+#awk '!(/^[ \t]*<\/tr>$/ && a~/<\/tr>$/){print}{a=$0}' < tmp.html > $OPFX.html
 
 if [ ! -e ${OPFX}_teams.yaml ]; then
     echo "You have no teams file, trying to generate one from the results"
@@ -18,7 +20,7 @@ if [ ! -e ${OPFX}_teams.yaml ]; then
     fi
 fi
 
-ruby preparse.rb $OPFX.html $OPFX.yaml
+ruby preparse.rb $OPFX.html $OPFX.yaml 2> weekly/$PPFX/parse
 LEAGUE=$OPFX.yaml PREFIX=$PPFX ./driver.sh
 HTML=1 ruby showfinal.rb history/$PPFX/* > $OHTML/test.xhtml
 rsync -Cavz svg/$PPFX/ $OHTML/
